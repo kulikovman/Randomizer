95311c0fe6d55a42130faa04895f928507f37934
Maksim Kulikov <kulikovman@gmail.com>
2017-08-29 00:01:36 +0500

    Восстановление значений при запуске

--- app/src/main/java/ru/kulikovman/randomizer/MainActivity.java
+++ app/src/main/java/ru/kulikovman/randomizer/MainActivity.java
@@ -1,15 +1,44 @@
 package ru.kulikovman.randomizer;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mNumberField;
+    private Button mBigButton;
+
+    private SharedPreferences mSharedPref;
+
+    private int mLimit;
+    private int mRandomResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Инициализируем необходимые вью элементы
+        mNumberField = (EditText) findViewById(R.id.number_field);
+        mBigButton = (Button) findViewById(R.id.big_button);
+
+        // Получаем SharedPreferences
+        mSharedPref = getSharedPreferences(getString(R.string.pref_key), Context.MODE_PRIVATE);
+
+        // Востанавливаем последние значения лимита и кнопки
+        mLimit = mSharedPref.getInt(getString(R.string.number_field), 10);
+        mRandomResult = mSharedPref.getInt(getString(R.string.big_button), 1);
+
+        mNumberField.setText(String.valueOf(mLimit));
+        mBigButton.setText(String.valueOf(mRandomResult));
+
+        Log.d("myLog", "Восстановили значение лимита и кнопки: " + mLimit + " | " + mRandomResult);
     }
 
     public void getRandomNumber(View view) {
