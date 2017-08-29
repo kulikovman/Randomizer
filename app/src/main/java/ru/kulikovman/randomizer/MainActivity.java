df47e0ef41435b764fe857201627a5e5226b8a1f
Maksim Kulikov <kulikovman@gmail.com>
2017-08-29 15:14:43 +0500

    Воспроизведение звука переделано под SoundPool

--- app/src/main/java/ru/kulikovman/randomizer/MainActivity.java
+++ app/src/main/java/ru/kulikovman/randomizer/MainActivity.java
@@ -1,8 +1,13 @@
 package ru.kulikovman.randomizer;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,11 +19,12 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences mSharedPref;
     private EditText mNumberField;
     private Button mBigButton;
 
-    private SharedPreferences mSharedPref;
-    private MediaPlayer mediaPlayer;
+    private SoundPool mSoundPool;
+    private int mBigButtonSound;
 
     private int mLimit;
     private int mRandomResult;
@@ -42,6 +48,39 @@ public class MainActivity extends AppCompatActivity {
         mBigButton.setText(String.valueOf(mRandomResult));
 
         Log.d("myLog", "Восстановили значения лимита и результата: " + mLimit + "|" + mRandomResult);
+
+        // Создаем SoundPool в зависимости от используемой версии Android
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            createNewSoundPool();
+        } else {
+            createOldSoundPool();
+        }
+
+        Log.d("myLog", "Создали SoundPool");
+
+        // Сохраняем id звукового файла
+        mBigButtonSound = mSoundPool.load(this, R.raw.tap_button, 1);
+
+        Log.d("myLog", "Сохранили id звукового файла");
+    }
+
+    // Создаем SoundPool для Android API 21 и выше
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    protected void createNewSoundPool() {
+        AudioAttributes attributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+
+        mSoundPool = new SoundPool.Builder()
+                .setAudioAttributes(attributes)
+                .build();
+    }
+
+    // Создаем SoundPool для старых версий Android
+    @SuppressWarnings("deprecation")
+    protected void createOldSoundPool() {
+        mSoundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
     }
 
     @Override
@@ -56,12 +95,17 @@ public class MainActivity extends AppCompatActivity {
         editor.apply();
 
         Log.d("myLog", "Сохранили значения лимита и результата: " + mLimit + "|" + mRandomResult);
+
+        // Очищаем SoundPool
+        mSoundPool.release();
+        mSoundPool = null;
+
+        Log.d("myLog", "SoundPool очищен");
     }
 
     public void getRandomNumber(View view) {
         // Воспроизводим звук короткого щелчка при нажатии
-        mediaPlayer = MediaPlayer.create(this, R.raw.tap_button);
-        mediaPlayer.start();
+        mSoundPool.play(mBigButtonSound, 1, 1, 1, 0, 1);
 
         // Генерируем случайное число в заданном пределе
         Random random = new Random();
