55c862cb6a2c9d85172013b1d185aad32ae038e4
Maksim Kulikov <kulikovman@gmail.com>
2018-06-12 23:12:47 +0500

    Небольшие правки

--- app/src/main/java/ru/kulikovman/randomizer/NumberPickerDialog.java
+++ app/src/main/java/ru/kulikovman/randomizer/NumberPickerDialog.java
@@ -1,6 +1,8 @@
 package ru.kulikovman.randomizer;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -11,55 +13,57 @@ import android.view.View;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
+import java.util.Objects;
+
 public class NumberPickerDialog extends DialogFragment {
     private TextView mStartLimitField, mEndLimitField;
     private int mStartLimit, mEndLimit;
 
-
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Log.d("log", "NumberPickerDialog запущен");
 
         // Получаем поля лимитов и их значения
-        mStartLimitField = getActivity().findViewById(R.id.start_limit_field);
-        mEndLimitField = getActivity().findViewById(R.id.end_limit_field);
+        mStartLimitField = Objects.requireNonNull(getActivity()).findViewById(R.id.start_limit_field);
+        mEndLimitField = Objects.requireNonNull(getActivity()).findViewById(R.id.end_limit_field);
 
         mStartLimit = Integer.parseInt(mStartLimitField.getText().toString());
         mEndLimit = Integer.parseInt(mEndLimitField.getText().toString());
 
         // Привязка к диалогу кастомного макета
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View numberPickerLayout = inflater.inflate(R.layout.number_picker_layout, null);
+        @SuppressLint("InflateParams") View numberPicker = inflater.inflate(R.layout.number_picker, null);
 
         // Настраиваем NumberPicker 1
-        final NumberPicker numberPicker1 = numberPickerLayout.findViewById(R.id.number_picker_1);
-        numberPicker1.setMinValue(0);
-        numberPicker1.setMaxValue(9);
-        numberPicker1.setWrapSelectorWheel(true);
+        final NumberPicker picker1 = numberPicker.findViewById(R.id.picker_1);
+        picker1.setMinValue(0);
+        picker1.setMaxValue(9);
+        picker1.setWrapSelectorWheel(true);
 
         // Настраиваем NumberPicker 2
-        final NumberPicker numberPicker2 = numberPickerLayout.findViewById(R.id.number_picker_2);
-        numberPicker2.setMinValue(0);
-        numberPicker2.setMaxValue(9);
-        numberPicker2.setWrapSelectorWheel(true);
+        final NumberPicker picker2 = numberPicker.findViewById(R.id.picker_2);
+        picker2.setMinValue(0);
+        picker2.setMaxValue(9);
+        picker2.setWrapSelectorWheel(true);
 
         // Настраиваем NumberPicker 3
-        final NumberPicker numberPicker3 = numberPickerLayout.findViewById(R.id.number_picker_3);
-        numberPicker3.setMinValue(0);
-        numberPicker3.setMaxValue(9);
-        numberPicker3.setWrapSelectorWheel(true);
+        final NumberPicker picker3 = numberPicker.findViewById(R.id.picker_3);
+        picker3.setMinValue(0);
+        picker3.setMaxValue(9);
+        picker3.setWrapSelectorWheel(true);
 
         // Создаем диалог
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Выбор")
-                .setMessage("Максимального значения")
-                .setView(numberPickerLayout)
+        builder.setTitle(R.string.dialog_title)
+                .setMessage(R.string.dialog_max_message)
+                .setView(numberPicker)
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        int num1 = numberPicker1.getValue();
-                        int num2 = numberPicker2.getValue();
-                        int num3 = numberPicker3.getValue();
+                        int num1 = picker1.getValue();
+                        int num2 = picker2.getValue();
+                        int num3 = picker3.getValue();
 
                         // Получаем выбранное число
                         int combinedValue = getCombinedValue(num1, num2, num3);
